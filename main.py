5eae784c6075f853b4659fd02b5478b75a6c6cd0
Deivison de alcantara santos <Deivisonalc02@gmail.com>
2020-08-12 20:37:02 -0300

    gerenciamento de tela concluida

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets 
+from PyQt5.QtWidgets import QApplication, QDesktopWidget
 from TelaInicial import Ui_MainWindow
 import player
+from banco import banco
 
 class main(QtWidgets.QMainWindow, Ui_MainWindow, player.player):
 
@@ -8,12 +10,17 @@ class main(QtWidgets.QMainWindow, Ui_MainWindow, player.player):
         super().__init__()
         self.setupUi(self)
         player.player.__init__(self)
+        self.bancoDeDados = banco()
         
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     ui = main()
+    display_monitor = len(QtGui.QGuiApplication.screens())
+    monitor = QDesktopWidget().screenGeometry(0)
+    ui.move(monitor.left(), monitor.top())
     ui.show()
     sys.exit(app.exec_())
+
 
--- player.py
+++ player.py
@@ -1,20 +1,18 @@
 import vlc 
-from PyQt5 import QtWidgets,QtGui
+from PyQt5 import QtWidgets,QtGui 
 from TelaInicial import Ui_MainWindow
-import time
-import asyncio
-from banco import banco
+from PyQt5.QtWidgets import QApplication, QDesktopWidget
 from PyQt5.QtCore import pyqtSlot, QTimer
 
     
 class player():
 
     def __init__(self):
-        #banco de dados
-        self.bancoDeDados = banco()
+        #Tela secundaria
+        self.telaSecundaria = QtWidgets.QWidget()
 
+        #Tela principal
         self.listaDeMidia = []
-
         self.botaoAdicionar.clicked.connect(self.adicionarExterno) # conector botaoAdicionar para função adicionarExterno
         self.botaoRemover.clicked.connect(self.removerItemDePlayList) # conector botaoRemover para função removerItemDePlayList
         self.listaPlaylist.itemClicked.connect(self.itemclicadoPlayList)  # conector listaPlaylist para função removerItemDePlayList
@@ -23,39 +21,44 @@ class player():
         self.botaoPlay.clicked.connect(self.play)
         self.botaoStop.clicked.connect(self.stop)
         self.slideVolume.sliderMoved.connect(self.volume)
-
         self.slideMusica.sliderPressed.connect(self.slidePressed)
         self.slideMusica.sliderReleased.connect(self.slideReleased)
         self.slideMusica.sliderMoved.connect(self.mudartempo)
         self.comboMusica.activated.connect(self.mudarFaixaDeAudio)
         self.comboLegenda.activated.connect(self.mudarFaixaDeLegenda)
-
         self.areaBusca.returnPressed.connect(self.busca)
         self.listaBanco.itemDoubleClicked.connect(self.midiaParaReproduzirVindoDoBanco)
         self.botaoAdicionar_2.clicked.connect(self.itemDoBancoParaPlaylist)
         self.listaBanco.itemClicked.connect(self.itemClicadoBanco)
+        self.botaoRedimencionar.clicked.connect(self.redimencionar)
 
-        #self.botaoProximo.clicked.connect(self.informacaoMidia)
+        self.slideMusicaPressionado = 0
 
 
+        #reprodutor
         self.player = vlc.Instance()
-
         self.mediaList = self.player.media_list_new()
-        self.reprodutorInstance = self.player.media_player_new()
+        
         self.reprodutorInstance2 = self.player.media_player_new()
+        self.reprodutorInstance2.set_hwnd(self.telaSecundaria.winId())
+
+        self.reprodutorInstance = self.player.media_player_new()
         self.reprodutorInstance.set_hwnd(self.frameVideo.winId())
 
-        #self.reprodutorInstance.set_media_list(self.mediaList)
+        #lista a tela secundaria do sistema
 
+        display_monitor = len(QtGui.QGuiApplication.screens())
+        monitor = QDesktopWidget().screenGeometry(1)
+        self.telaSecundaria.move(monitor.left(), monitor.top())
+        
+        #Eventos
         event_manager = self.reprodutorInstance.event_manager()
         event_manager.event_attach(vlc.EventType.MediaPlayerPlaying, self.reproduzindo)
         event_manager.event_attach(vlc.EventType.MediaPlayerMuted, self.mute)
         event_manager.event_attach(vlc.EventType.MediaPlayerPositionChanged,self.tempo)
         event_manager.event_attach(vlc.EventType.MediaPlayerEndReached,self.fimdaReproducao)
-        #event_manager.event_attach(vlc.EventType.MediaPlayerBuffering,self.teste)
-
-        self.slideMusicaPressionado = 0
-
+        
+        #Timer
         self.timer = QTimer(self)
         
     def adicionarExterno(self):
@@ -76,6 +79,7 @@ class player():
             
             print(self.listaDeMidia)
 
+
         except:
             print('não selecionou nenhum item')
                  
@@ -83,7 +87,7 @@ class player():
         self.itemClicado = item
     
     def midiaParaReproduzirVindoDaLista(self,arg):
-
+    
         arq = self.listaDeMidia[self.listaPlaylist.row(arg)] 
         self.reprodutor(arq)
     
@@ -96,28 +100,32 @@ class player():
         self.midia = self.player.media_new(arg) #caminho do arquivo para ser execultado
         self.midia.parse() # essaa função tem que ser execultada para que os dados possam ser obtidos
 
+        #segundo reprodutor
+        self.reprodutorInstance2.set_hwnd(self.telaSecundaria.winId())
+        self.reprodutorInstance2.set_media(self.midia)
+        self.reprodutorInstance2.play()
+        self.reprodutorInstance2.audio_set_mute(True)
+
+        self.telaSecundaria.setVisible(True)
+        self.telaSecundaria.showFullScreen()
+
+        #primeiro reprodutor
         self.reprodutorInstance.set_media(self.midia)
-
         self.reprodutorInstance.play()
-
+        self.reprodutorInstance.set_hwnd(self.frameVideo.winId())
+        
+        
         self.timer.timeout.connect(self.informacaoMidia)
         self.timer.start(500)
 
-        '''
-        #segundo reprodutor
-        self.reprodutorInstance2.set_media(self.midia)
-        self.reprodutorInstance2.play()
-
-        '''
-    
     def reproduzindo(self,event):
 
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap("icones/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.botaoPlay.setIcon(icon1)
+        #self.telaSecundaria.show()
     
     def informacaoMidia(self):
-        print('pegou')
         self.audios = self.reprodutorInstance.audio_get_track_description()
         self.legendas = self.reprodutorInstance.video_get_spu_description()
 
@@ -140,25 +148,37 @@ class player():
         self.timer.stop()
 
     def play(self):
-
+        
         if self.reprodutorInstance.is_playing():
             self.reprodutorInstance.pause()
+            self.reprodutorInstance2.pause()
             icon1 = QtGui.QIcon()
             icon1.addPixmap(QtGui.QPixmap("icones/play.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
             self.botaoPlay.setIcon(icon1)
         else:
             self.reprodutorInstance.play()
+            self.reprodutorInstance2.play()
             self.timer.timeout.connect(self.informacaoMidia)
             self.timer.start(500)
    
     def stop(self):
-        self.reprodutorInstance.stop()
+        
+        self.reprodutorInstance.stop() #para player principal
+
+
+        self.telaSecundaria.setVisible(False)
+        self.reprodutorInstance2.set_position(0) #o player da tela secundaria não da stop e sim fica invisivel na posição 0
+        self.reprodutorInstance2.pause()
+        
+
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap("icones/play.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.botaoPlay.setIcon(icon1)
         self.slideMusica.setValue(0)
+
         self.comboLegenda.clear()
         self.comboMusica.clear()
+        self.botaoRedimencionar.setChecked(False)
 
     def mute(self,event):
         icon1 = QtGui.QIcon()
@@ -190,24 +210,29 @@ class player():
     def mudartempo(self,arg):
 
         self.reprodutorInstance.set_position(arg/100)
+        self.reprodutorInstance2.set_position(arg/100)
 
     def fimdaReproducao(self,event):
         self.slideMusica.setValue(0)
     
     def mudarFaixaDeAudio(self,arg):
-
+        print('pegou')
         if arg == 0:
-            self.reprodutorInstance.audio_set_track(-1) # é usado -1 porque o vlc usa esse indice para desabilitar o audio
+            self.reprodutorInstance.audio_set_track(-1)
+            self.reprodutorInstance2.audio_set_track(-1) # é usado -1 porque o vlc usa esse indice para desabilitar o audio
 
         else:
             self.reprodutorInstance.audio_set_track(arg)
+            self.reprodutorInstance2.audio_set_mute(True)
     
     def mudarFaixaDeLegenda(self,arg):
 
         if arg == 0:
-            self.reprodutorInstance.video_set_spu(-1) # é usado -1 porque o vlc usa esse indice para desabilitar o audio
+            self.reprodutorInstance.video_set_spu(-1)
+            self.reprodutorInstance2.video_set_spu(-1) # é usado -1 porque o vlc usa esse indice para desabilitar o audio
         else:
             self.reprodutorInstance.video_set_spu(arg)
+            self.reprodutorInstance2.video_set_spu(arg)
     
     def busca(self):
 
@@ -235,5 +260,11 @@ class player():
     def itemClicadoBanco(self,arg):
         self.itemBanco = arg
     
-        
+    def redimencionar(self,arg):
+        print(arg)
+        if arg:
+            self.telaSecundaria.showMaximized()
+        else:
+            self.telaSecundaria.setVisible(False)
+
         
